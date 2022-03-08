ddbc479fc42bd45c28a118f2d099d2a3491addc8
theRabbi <tipson664@gmail.com>
2022-03-08 12:34:48 +0100

    new commit

--- src/routes/web/web.router.js
+++ src/routes/web/web.router.js
@@ -3,4 +3,4 @@ const siteRouter = require('./site.router');
 const webRouter = require('express').Router();
 //all router on web router will be use here for prefixing
 webRouter.use('/', siteRouter)
-module.exports=webRouter;
+ module.exports=webRouter;
