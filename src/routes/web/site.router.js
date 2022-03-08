90e1b37c807ed95502accae562cd8d7219391bb3
funmibi <fh.iyanda@gmail.com>
2022-03-08 12:39:18 +0000

    nigsims

--- src/routes/web/site.router.js
+++ src/routes/web/site.router.js
@@ -5,7 +5,11 @@ const siteRouter = require('express').Router();
 siteRouter.get('/home', (req, res)=>{
     let y = siteController.login(req, res);
     y.then(r=>{
-        res.render('home', r);
+        res.render('home', {r:r});
+         
+    }, e={
+
     })
 })
+siteRouter.get('/', siteController.home)
 module.exports=siteRouter;
