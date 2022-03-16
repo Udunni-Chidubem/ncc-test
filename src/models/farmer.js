446a8125be2fcf41c40e0e3a533eda5ccb27dbf1
Oyedele Olufemi <itz4mesays@gmail.com>
2022-03-16 13:16:58 +0100

    migration for user,user_role,role,farmer

---
+++ src/models/farmer.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const user = require('./user');
+module.exports = (sequelize, DataTypes) => {
+  class Farmer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Farmer.belongsTo(user)
+    }
+  }
+  Farmer.init({
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    othername: DataTypes.STRING,
+    product_farmed: DataTypes.STRING,
+    location_of_farm: DataTypes.STRING,
+    size_of_farm: DataTypes.STRING,
+    phone_no: DataTypes.STRING,
+    bvn: DataTypes.STRING,
+    account_name: DataTypes.STRING,
+    nin: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    level_of_education: DataTypes.STRING,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Farmer',
+  });
+  return Farmer;
+};
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const farmer = require('./farmer');
+const user_role = require('./user_role');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(user_role)
+      User.hasOne(farmer)
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
