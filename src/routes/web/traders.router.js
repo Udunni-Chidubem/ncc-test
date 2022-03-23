c96fb98e6570d778d85b896c66916e02c5342dac
Oyedele Olufemi <itz4mesays@gmail.com>
2022-03-23 11:45:34 +0100

    restructing views

--- src/routes/web/traders.router.js
+++ src/routes/web/traders.router.js
@@ -4,8 +4,8 @@ tradersRouter.get('/dashboard', async (req, res)=>{
 
     //let farmer = await farmerController.dashboard(req, res);
     res.render('seed_trader/dashboard', {
-        layout : 'traders-dashboard'
-       // farmer : farmer
+        layout : 'traders-dashboard',
+        title : 'Dashboard'
     })
 })
 module.exports=tradersRouter
