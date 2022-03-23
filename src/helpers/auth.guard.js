58abb7b25800b4e6690ca8653199cae957060a0d
theRabbi <tipson664@gmail.com>
2022-03-23 13:26:30 +0100

    new commit

--- src/helpers/auth.guard.js
+++ src/helpers/auth.guard.js
@@ -9,7 +9,6 @@ module.exports = {
 
     //this check if you have a session and will be called on the login route
     loggedIn: (req, res, next)=>{
-        console.log(req.user)
         if(req.isAuthenticated()){
            res.redirect('/dashboard');
         }else{
@@ -20,7 +19,7 @@ module.exports = {
 
     //this will help handle redirects
     redirect : (req, res, role)=>{
-           // console.log(req.user)
+           req.flash('user', req.user)
            if( role == 'farmer')
                 res.redirect('farmer/dashboard')
             
