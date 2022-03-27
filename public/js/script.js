90f1f15ab631738cc584c1830650f99a5b4b09d4
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-03-27 11:28:55 +0100

    Working on the Seed Details Section under market place

--- public/js/script.js
+++ public/js/script.js
@@ -32,6 +32,41 @@ sidebarBtn.addEventListener("click", ()=>{
   // showSpan.classList.add('show');
 });
 
+$(document).ready(function(){
+
+var quantitiy=0;
+   $('.quantity-right-plus').click(function(e){
+        
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+            
+            $('#quantity').val(quantity + 1);
+
+          
+            // Increment
+        
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+      
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+    
+});
+
 let activeBtn = document.querySelector("activeB")
 activeBtn.addEventListener("click", ()=> {
   
@@ -51,3 +86,4 @@ function decreaseValue() {
   value--;
   document.getElementById('number').value = value;
 }
+
