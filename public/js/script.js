730f54c589041c076319c7250755e176566ddea7
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-03-27 12:13:55 +0100

    Working on the star rating

--- public/js/script.js
+++ public/js/script.js
@@ -64,6 +64,11 @@ var quantitiy=0;
             $('#quantity').val(quantity - 1);
             }
     });
+
+     $("input[type='radio']").click(function(){
+var sim = $("input[type='radio']:checked").val();
+//alert(sim);
+if (sim<3) { $('.myratings').css('color','red'); $(".myratings").text(sim); }else{ $('.myratings').css('color','green'); $(".myratings").text(sim); } });
     
 });
 
