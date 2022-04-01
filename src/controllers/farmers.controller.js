7bf5a47932b8784e152031adbe842ccd07b2ad5c
Oyedele Olufemi <itz4mesays@gmail.com>
2022-04-01 08:49:15 +0100

    minor fix

--- src/controllers/farmers.controller.js
+++ src/controllers/farmers.controller.js
@@ -59,7 +59,7 @@ module.exports={
             const data = {
                 firstname, 
                 lastname, 
-                date_of_birth: date_of_birth ? date_of_birth : null, 
+                date_of_birth: date_of_birth ? date_of_birth : '0000-00-00 00:00:00', 
                 gender, 
                 level_of_education, 
                 state_id, 
