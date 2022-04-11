f92c5228a74370f2b06506f83f960199c06c2708
hopeanthonie <hopelijah98@gmail.com>
2022-04-11 14:29:23 +0100

    modifications to the product-mgt-screen

--- src/routes/web/admin.router.js
+++ src/routes/web/admin.router.js
@@ -1,38 +1,72 @@
+// const adminRouter=require('express').Router()
+// const siteController = require('../../controllers/site.controller');
+// const utils = require('../../helpers/utils')
+// const { adminValidation, validate, productValidation } = require('../../helpers/formValidator');
+// const adminController = require('../../controllers/admin.controller');
+
+// adminRouter.get('/dashboard', async (req, res)=>{
+//     let user = await req.user
+
+//     let isVerified = await utils.isVerified(user)
+    
+//     res.render('admin/dashboard', {
+//         layout : 'admin-dashboard',
+//         title : 'Dashboard',
+//         isVerified
+//     })
+// });
+
+// adminRouter.get('/create-user', async (req, res)=>{
+//     let user = await req.user
+
+//     let isVerified = await utils.isVerified(user)
+    
+//     res.render('admin/product-mgt', {
+//         layout : 'admin-dashboard',
+//         title : 'Product Management',
+//     },
+//     res.render('admin/create-user', {
+//         layout : 'admin-dashboard',
+//         title : 'Dashboard',
+
+//         isVerified
+//     })
+// });
+
+
+
+// module.exports = adminRouter
+
 const adminRouter=require('express').Router()
 const siteController = require('../../controllers/site.controller');
 const utils = require('../../helpers/utils')
 const { adminValidation, validate, productValidation } = require('../../helpers/formValidator');
 const adminController = require('../../controllers/admin.controller');
-
 adminRouter.get('/dashboard', async (req, res)=>{
     let user = await req.user
-
     let isVerified = await utils.isVerified(user)
-    
     res.render('admin/dashboard', {
         layout : 'admin-dashboard',
         title : 'Dashboard',
         isVerified
     })
 });
-
 adminRouter.get('/create-user', async (req, res)=>{
     let user = await req.user
-
     let isVerified = await utils.isVerified(user)
-     HEAD
-    res.render('admin/product-mgt', {
-        layout : 'admin-dashboard',
-        title : 'Product Management',
-
     res.render('admin/create-user', {
         layout : 'admin-dashboard',
         title : 'Dashboard',
-
         isVerified
     })
 });
-
-
-
+adminRouter.get('/product-mgt', async (req, res)=>{
+    let user = await req.user
+    let isVerified = await utils.isVerified(user)
+    res.render('admin/product-mgt', {
+        layout : 'admin-dashboard',
+        title : 'Product Management',
+        isVerified
+    })
+});
 module.exports = adminRouter
