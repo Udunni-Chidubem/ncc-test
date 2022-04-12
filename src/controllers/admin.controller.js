f67b7a85edaa891a4e3041594cf671f9f15e51e7
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-04-12 15:41:18 +0100

    Working on the view user page on the admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -22,7 +22,7 @@ module.exports={
     }, 
     getFarmers: async (req, res) =>{
         let farmers = await Farmer.findAll({
-            attributes : ['firstname', 'lastname', 'created_at'],
+            attributes : ['id', 'firstname', 'lastname', 'created_at'],
             include : [ 
                 {
                     model : States,
@@ -32,9 +32,15 @@ module.exports={
                     model : LGAs,
                     attributes : ['name']
                 }
+            ],
+            order : [
+            	['created_at', 'DESC']
             ]
          })
          farmers=JSON.stringify(farmers)
+
+         console.log(farmers)
+
         return JSON.parse(farmers)
     },
     getCompanies : async (req, res)=>{
@@ -48,6 +54,9 @@ module.exports={
                     model : LGAs,
                     attributes : ['name']
                 }
+            ],
+            order : [
+            	['created_at', 'DESC']
             ]
         })
 
@@ -65,10 +74,41 @@ module.exports={
                     model : LGAs,
                     attributes : ['name']
                 }
+            ],
+            order : [
+            	['created_at', 'DESC']
             ]
         })
         traders = JSON.stringify(traders)
         return JSON.parse(traders)
+    },
+
+    viewFarmer: async (req, res) => {
+        const user = await req.user
+        let response = null
+
+        const singleFarmer = await Farmer.findOne({
+            where: {id: req.params.id},
+            attributes : ['id', 'firstname', 'lastname','gender', 'date_of_birth', 'level_of_education', 'nin', 'bvn', 'phone_no', 'state_id', 'created_at'],
+            include : [ 
+                {
+                    model : States,
+                    attributes : ['name']
+                },
+                {
+                    model : LGAs,
+                    attributes : ['name']
+                }
+            ],
+            raw: true
+        })
+
+         if(singleFarmer){
+            response = JSON.parse(JSON.stringify(singleFarmer))
+            console.log(response)
+        }
+
+        return response
     }
 
 
--- src/routes/web/admin.router.js
+++ src/routes/web/admin.router.js
@@ -8,6 +8,7 @@ const adminController = require('../../controllers/admin.controller');
 adminRouter.get('/dashboard', async (req, res)=>{
     let user = await req.user
     let isVerified = await utils.isVerified(user)
+
     res.render('admin/dashboard', {
         layout : 'admin-dashboard',
         title : 'Dashboard',
@@ -19,13 +20,14 @@ adminRouter.get('/dashboard', async (req, res)=>{
 adminRouter.get('/create-user', async (req, res)=>{
     let user = await req.user
     let roles =await adminController.getNascAdminRoles(req, res)
-   // console.log(user.username)
     let isVerified = await utils.isVerified(user)
+
     res.render('admin/create-user', {
         layout : 'admin-dashboard',
         title : 'User Management',
         sub_title : 'Create User',
         username : user.username,
+        prev_link: '/admin/all-users',
         isVerified,
         roles : roles
     })
@@ -34,14 +36,17 @@ adminRouter.get('/create-user', async (req, res)=>{
 
 adminRouter.get('/all-users', async (req, res)=>{
     let user = await req.user
-     let farmers=await adminController.getFarmers(req, res)
-     let traders = await adminController.getTraders(req, res)
-     let companies= await adminController.getCompanies(req, res)
+    let farmers=await adminController.getFarmers(req, res)
+    let traders = await adminController.getTraders(req, res)
+    let companies= await adminController.getCompanies(req, res)
     let isVerified = await utils.isVerified(user)
+
+
     
     res.render('admin/all-users', {
         layout : 'admin-dashboard',
-        title : 'All Users',
+        title : 'User Management',
+        sub_title : 'All Users',
         username : user.username,
         isVerified,
         farmers,
@@ -52,13 +57,13 @@ adminRouter.get('/all-users', async (req, res)=>{
 
 adminRouter.get('/view-product', async (req, res)=>{
     let user = await req.user
-
     let isVerified = await utils.isVerified(user)
     
     res.render('admin/view-product', {
         layout : 'admin-dashboard',
         title : 'Product Management',
         sub_title : 'View Product',
+        prev_link: '/admin/product-mgt',
         username : user.username,
         isVerified
     })
@@ -67,10 +72,35 @@ adminRouter.get('/view-product', async (req, res)=>{
 adminRouter.get('/product-mgt', async (req, res)=>{
     let user = await req.user
     let isVerified = await utils.isVerified(user)
+
     res.render('admin/product-mgt', {
         layout : 'admin-dashboard',
         title : 'Product Management',
+        username : user.username,
         isVerified
     })
 });
+
+adminRouter.get('/view-user/:id', async (req, res)=>{
+    let user = await req.user
+    let farmer=await adminController.viewFarmer(req, res)
+    // let traders = await adminController.getTraders(req, res)
+    // let companies= await adminController.getCompanies(req, res)
+    let isVerified = await utils.isVerified(user)
+
+    
+    
+    res.render('admin/view-user', {
+        layout : 'admin-dashboard',
+        title : 'View User',
+        sub_title : 'View User',
+        prev_link : '/admin/all-users',
+        username : user.username,
+        isVerified,
+        farmer
+        // companies,
+        // traders
+    })
+});
+
 module.exports = adminRouter
