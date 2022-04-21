08cd562798929555dc7e37e152348a9e2523d712
funmibi <fh.iyanda@gmail.com>
2022-04-21 12:47:30 +0100

    added link to related product

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { Op, QueryTypes } = require("sequelize");
 const db = require('../models')
-const {User,UserRole, Role, SeedCompany,DeliveryInformation, LGAs, States, Product, Wallet, TransactionLog, TransactionCarts, Cart, Farmer}  = db
+const { User, UserRole, Role, SeedCompany, DeliveryInformation, LGAs, States, Product, Wallet, TransactionLog, TransactionCarts, Cart, Farmer } = db
 const utils = require('../helpers/utils');
 const { getPagingData, getPagination } = require('../helpers/pagination');
 
@@ -12,244 +12,244 @@ module.exports = {
         const transaction = await db.rest.transaction();
         const user = await req.user
 
-        const { name_of_company, phone_no, tin, address, licensed_no, state_id, lg_id, certification_number, email} = req.body
+        const { name_of_company, phone_no, tin, address, licensed_no, state_id, lg_id, certification_number, email } = req.body
 
-        try{
+        try {
             const data = { name_of_company, phone_no, tin, address, licensed_no, state_id, lg_id, certification_number, email }
 
-            await User.update({status: true}, {where: {id: user.id} })
+            await User.update({ status: true }, { where: { id: user.id } })
 
-            const company = await SeedCompany.update( data , {
+            const company = await SeedCompany.update(data, {
                 where: { user_id: user.id }
-            }, {transaction: transaction})
+            }, { transaction: transaction })
 
             transaction.commit();
-            return {company}
-        }catch(e){
+            return { company }
+        } catch (e) {
             transaction.rollback();
             return e
         }
     },
-    createProduct : async (req, res, filename)=>{
-        
-        const transaction =await db.rest.transaction();
-        let min=[], qty=[], size=[], price=[];
-        if(!Array.isArray(req.body.min_order)){
+    createProduct: async (req, res, filename) => {
+
+        const transaction = await db.rest.transaction();
+        let min = [], qty = [], size = [], price = [];
+        if (!Array.isArray(req.body.min_order)) {
             min.push(req.body.min_order)
             size.push(req.body.pkg_size)
             qty.push(req.body.quantity)
             price.push(req.body.price)
-        }else{
-            min=req.body.min_order
-            size=req.body.pkg_size
-            qty=req.body.quantity
-            price=req.body.price
+        } else {
+            min = req.body.min_order
+            size = req.body.pkg_size
+            qty = req.body.quantity
+            price = req.body.price
         }
         let item = {
-            min : min, 
-            pkg: size, 
-            price:price, 
-            quantity : qty
+            min: min,
+            pkg: size,
+            price: price,
+            quantity: qty
         }
 
         item = await JSON.stringify(item, null, 2)
 
         const user = await req.user
-        try{
+        try {
             let p = await Product.create({
-                product_name : req.body.productName,
-                description : req.body.productDescription,
-                variant : req.body.productVariant,
-                item : item,
-                user_id : user.id,
-                file_name : filename
-            }, {transaction : transaction})
+                product_name: req.body.productName,
+                description: req.body.productDescription,
+                variant: req.body.productVariant,
+                item: item,
+                user_id: user.id,
+                file_name: filename
+            }, { transaction: transaction })
             transaction.commit()
             return p
-        }catch(e){
+        } catch (e) {
             transaction.rollback()
             return e
         }
-        
+
     },
     listProducts: async (req, res) => {
         const user = await req.user
         let response = null
 
-        const {page, size} = req.query
-        const {limit, offset} = getPagination(page, size)
+        const { page, size } = req.query
+        const { limit, offset } = getPagination(page, size)
 
-        const product = await Product.findAndCountAll({ 
-            where: { user_id: user.id},
+        const product = await Product.findAndCountAll({
+            where: { user_id: user.id },
             order: [
                 ['id', 'DESC'],
             ],
-            attributes: ['id','product_name', 'variant', 'description', 'item', 'file_name', 'status'],
-            raw: true, limit, offset 
+            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name', 'status'],
+            raw: true, limit, offset
         })
 
-        if(product){
+        if (product) {
             response = getPagingData(product, page, limit)
         }
         return response
     },
-    updateProduct : async (req, res)=>{
-        const transaction =await db.rest.transaction();
-        try{
-             let min=[], qty=[], size=[], price=[];
-            if(req.files){
+    updateProduct: async (req, res) => {
+        const transaction = await db.rest.transaction();
+        try {
+            let min = [], qty = [], size = [], price = [];
+            if (req.files) {
                 let p = await Product.findOne({
-                    attributes : file_name,
-                    where: {id : req.params.id}
+                    attributes: file_name,
+                    where: { id: req.params.id }
                 })
-                let upload=req.files.upload
-                filename=p.file_name
-                upload.mv('./public/product_images/'+filename)
+                let upload = req.files.upload
+                filename = p.file_name
+                upload.mv('./public/product_images/' + filename)
             }
-            if(!Array.isArray(req.body.min_order)){
+            if (!Array.isArray(req.body.min_order)) {
                 min.push(req.body.min_order)
                 size.push(req.body.pkg_size)
                 qty.push(req.body.quantity)
                 price.push(req.body.price)
-            }else{
-                min=req.body.min_order
-                size=req.body.pkg_size
-                qty=req.body.quantity
-                price=req.body.price
+            } else {
+                min = req.body.min_order
+                size = req.body.pkg_size
+                qty = req.body.quantity
+                price = req.body.price
             }
             let item = {
-                min : min, 
-                pkg: size, 
-                price:price, 
-                quantity : qty
+                min: min,
+                pkg: size,
+                price: price,
+                quantity: qty
             }
             item = await JSON.stringify(item, null, 2)
             Product.update(
                 {
-                    product_name : req.body.productName,
-                    description : req.body.productDescription,
-                    variant : req.body.productVariant,
-                    item : item
-                }, 
-                {
-                    where : {id : req.params.id}
+                    product_name: req.body.productName,
+                    description: req.body.productDescription,
+                    variant: req.body.productVariant,
+                    item: item
                 },
                 {
-                    transaction :transaction
+                    where: { id: req.params.id }
+                },
+                {
+                    transaction: transaction
                 }
             )
             transaction.commit()
-        }catch(e){
+        } catch (e) {
             console.log(e)
             transaction.rollback
         }
-        
+
     },
     viewProduct: async (req, res) => {
         const user = await req.user
         let response = null
 
         const singleProduct = await Product.findOne({
-            where: { user_id: user.id, id: req.params.id},
-            attributes: ['id','product_name', 'variant', 'description', 'item', 'file_name'],
+            where: { user_id: user.id, id: req.params.id },
+            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name'],
             raw: true
         })
 
-         if(singleProduct){
+        if (singleProduct) {
             response = singleProduct
         }
 
         return response
     },
-    creditWallet : async (carts)=>{
+    creditWallet: async (carts) => {
         let transaction = await db.rest.transaction()
-        try{
-           // let transaction = db.rest.transaction()
-            carts.forEach(async cart=>{
-            await Wallet.increment({
-                    amount : cart.total_amount
+        try {
+            // let transaction = db.rest.transaction()
+            carts.forEach(async cart => {
+                await Wallet.increment({
+                    amount: cart.total_amount
                 }, {
-                    where : {user_id : cart.Product.user_id}
-                }, {transaction : transaction})
+                    where: { user_id: cart.Product.user_id }
+                }, { transaction: transaction })
             })
             transaction.commit()
-        }catch(e){
+        } catch (e) {
             transaction.rollback()
             console.log(e)
         }
-       
+
     },
-    getWallet : async (req, res)=>{
-    const user = await req.user
-       let balance 
-       const singleBalance = await Wallet.findOne({
-            where: {user_id: user.id},
-            attributes : ['amount'],
+    getWallet: async (req, res) => {
+        const user = await req.user
+        let balance
+        const singleBalance = await Wallet.findOne({
+            where: { user_id: user.id },
+            attributes: ['amount'],
             raw: true
         });
-        
+
         balance = singleBalance
         console.log(balance)
         return balance;
     },
-    getProductCount : async (req, res)=>{
+    getProductCount: async (req, res) => {
         const user = await req.user
         let productCount
-         const countProducts = await Product.count({
-            where: {user_id: user.id}
+        const countProducts = await Product.count({
+            where: { user_id: user.id }
         });
 
         productCount = countProducts
         console.log(productCount)
         return productCount;
     },
-    getOrders : async (req, res)=>{
+    getOrders: async (req, res) => {
         const user = await req.user
-        let sql="SELECT distinct tl.id, SUM(c.total_amount) as amount, tl.transaction_id, tl.created_at," 
-        +"tl.updated_at, tl.transaction_ref,f.firstname, f.lastname, c.status as order_status, "
-        +"tl.status as payment_status from cart c join transaction_carts tc on c.id = tc.cart_id join transaction_log tl " 
-        +"on tl.id=tc.transaction_log_id join product p on p.id = c.product_id join farmer f on f.user_id=c.user_id "
-        +"where p.user_id = "+user.id+" GROUP by p.user_id, tl.id order by tl.created_at desc";
-        let order= await db.rest.query(sql, { type: QueryTypes.SELECT } )
+        let sql = "SELECT distinct tl.id, SUM(c.total_amount) as amount, tl.transaction_id, tl.created_at,"
+            + "tl.updated_at, tl.transaction_ref,f.firstname, f.lastname, c.status as order_status, "
+            + "tl.status as payment_status from cart c join transaction_carts tc on c.id = tc.cart_id join transaction_log tl "
+            + "on tl.id=tc.transaction_log_id join product p on p.id = c.product_id join farmer f on f.user_id=c.user_id "
+            + "where p.user_id = " + user.id + " GROUP by p.user_id, tl.id order by tl.created_at desc";
+        let order = await db.rest.query(sql, { type: QueryTypes.SELECT })
         console.log(order)
         return order
     },
-    getOrder : async (transaction_id, user_id)=>{
-       // const user = await req.user
-        let order=await TransactionCarts.findAll({
-            include :[ 
+    getOrder: async (transaction_id, user_id) => {
+        // const user = await req.user
+        let order = await TransactionCarts.findAll({
+            include: [
                 {
-                    model : TransactionLog,
-                    where :{transaction_id : transaction_id}
+                    model: TransactionLog,
+                    where: { transaction_id: transaction_id }
                 },
                 {
-                    model : Cart,
-                    include : [ {
-                        model : Product,
-                        where : {user_id : user_id }
+                    model: Cart,
+                    include: [{
+                        model: Product,
+                        where: { user_id: user_id }
                     }]
                 }
             ]
         })
 
-        order =  JSON.parse(JSON.stringify(order))
+        order = JSON.parse(JSON.stringify(order))
         let farmer = await Farmer.findOne({
-            where : {id : order[0].TransactionLog.farmer_id},
-            include : [
+            where: { id: order[0].TransactionLog.farmer_id },
+            include: [
                 {
-                    model : User,
-                    include : [
+                    model: User,
+                    include: [
                         {
-                            model : DeliveryInformation,
-                            include:[{model : States}, {model:LGAs}]
+                            model: DeliveryInformation,
+                            include: [{ model: States }, { model: LGAs }]
                         }
                     ]
                 }
             ]
         })
-        farmer=JSON.parse(JSON.stringify(farmer))
+        farmer = JSON.parse(JSON.stringify(farmer))
         console.log(order)
         console.log(farmer)
-        return {order, farmer};
+        return { order, farmer };
     }
 }
