5c900d9c4f1f257eb303296f4fb3f1d023f60a29
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-04-24 12:28:16 +0100

    Added dropdown to services link, Fixed hamburger icon in login and sign up pages, added local name to product list in seed company, add the onhover info section in the create product page, adjusted the success message on the update profile page

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -86,7 +86,7 @@ module.exports = {
             order: [
                 ['id', 'DESC'],
             ],
-            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name', 'status'],
+            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name', 'status', 'local_name'],
             raw: true, limit, offset
         })
 
@@ -154,7 +154,7 @@ module.exports = {
 
         const singleProduct = await Product.findOne({
             where: { user_id: user.id, id: req.params.id },
-            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name'],
+            attributes: ['id', 'product_name', 'variant', 'description', 'item', 'file_name', 'local_name'],
             raw: true
         })
 
