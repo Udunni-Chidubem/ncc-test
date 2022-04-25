4b3878d2d5334b47186562cb7eab62293b96cd3e
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-04-25 09:29:21 +0100

    Working on the route

--- src/routes/web/company.router.js
+++ src/routes/web/company.router.js
@@ -163,7 +163,7 @@ companyRouter.get('/orders/:transaction_id/', async (req, res)=>{
     let {order, farmer, orderStatus} = await companyController.getOrder(req.params.transaction_id, user.id, company.id)
     res.render('seed_company/view-order', {
         layout : 'company-dashboard',
-        title : 'Order List',
+        title : 'Orders',
         sub_title : 'View Order',
         order,
         farmer,
@@ -186,8 +186,9 @@ companyRouter.post('/orders/:transaction_id/', async (req, res)=>{
     
     res.render('seed_company/view-order', {
         layout : 'company-dashboard',
-        title : 'Order Management',
+        title : 'Orders',
         sub_title : 'View Order',
+        prev_link : '/admin/orders',
         order,
         farmer,
         orderStatus,
