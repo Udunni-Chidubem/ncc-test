7bc8ff2f5e9deb13990a2c6618920df2c907e96c
Tochukwu Franklin Ene <teec3000@gmail.com>
2022-05-27 12:24:49 +0100

    Working on the referral Dashboard

--- public/js/script.js
+++ public/js/script.js
@@ -71,6 +71,21 @@ $(document).ready(function(){
 
   });
   
+$(document).ready(function CopyToClipboard(containerid) {
+  if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(containerid));
+      range.select().createTextRange();
+      document.execCommand("copy");
+  } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(containerid));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      alert("Code has been copied.")
+  }
+});
+
 $(document).ready(function() {
   $('#activity-log-table').DataTable({
     "order": [[2, "desc"]]
