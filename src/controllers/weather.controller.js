258435784fd1c15f00fdc8c6603cfac38042c2e3
theRabbi <tipson664@gmail.com>
2022-06-04 23:12:04 +0100

    new commit

--- src/controllers/weather.controller.js
+++ src/controllers/weather.controller.js
@@ -8,21 +8,23 @@ module.exports={
                 return e.LocalizedName==state || e.EnglishName==state
             })
             let url=`${process.env.wheather_base_url}/locations/v1/cities/neighbors/${cor_state[0].Key}?apikey=${process.env.wheather_api_key}`
-            console.log(url)
+           
             let res=await axios.get(url)
+            
             let lgs=locals.find(e=>{
                 return e.name==state
             });
             let lg=lgs.value.find(l=>{
-            return l.accu_name==local || l.name==local
+                return l.accu_name==local || l.name==local
             })
+            
             let nb=res.data.find(l=>{
-            return l.LocalizedName==lg.accu_name || l.LocalizedName==lg.name || l.EnglishName==lg.accu_name || l.EnglishName==lg.name
+                return l.LocalizedName==lg.accu_name || l.LocalizedName==lg.name || l.EnglishName==lg.accu_name || l.EnglishName==lg.name
             })
+            
             let url2=`${process.env.wheather_base_url}/forecasts/v1/daily/5day/${nb.Key}?apikey=${process.env.wheather_api_key}`
-            console.log(url2)
-            let fres=await axios.get(url)
-            return fres
+            let fres=await axios.get(url2)
+            return fres.data
         }catch(e){
             return e
         }
--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -8,7 +8,10 @@ const web = require('./web/web.router');
 
 router.use('/api', api);
 router.get('/weather',async (req, res)=>{
-    res.send(await weatherController.getCities(req.query.state, req.query.local))
+    
+    let y =await weatherController.getCities(req.query.state, req.query.local)
+    console.log(y)
+    res.send(y)
 })
 router.use('/', web)
 
