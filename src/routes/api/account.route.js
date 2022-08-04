fee6830d40335ae914972e3ed4f94ed30a441e23
theRabbi <tipson664@gmail.com>
2022-08-04 15:15:16 +0100

    added api for account signup and login

---
+++ src/routes/api/account.route.js
@@ -0,0 +1,63 @@
+const accountRouter = require("express").Router();
+const siteController = require("../../controllers/site.controller");
+
+accountRouter.post("/login", async (req, res) => {
+  let resp = await siteController.apiLogin(req);
+  res.status(resp.statusCode).json(resp);
+});
+
+accountRouter.post("/farmer-signup", async (req, res) => {
+  let y = await siteController.savefarmer(req, res);
+  if (r.user) {
+    res
+      .json({
+        statusCode: 200,
+        message: "Account created successfully",
+        body: r.farmer,
+      })
+      .send();
+  } else {
+    res
+      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .send();
+  }
+})
+accountRouter.post("/company-signup",  (req, res) => {
+ let y = await siteController.saveseedcompany(req, res)
+  if (r.user) {
+    res
+      .json({
+        statusCode: 200,
+        message: "Account created successfully",
+        body: r.seed_company
+      })
+      .send();
+  } else {
+    res
+      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .send();
+  }
+  }
+);
+
+accountRouter.post( "/trader-signup",  (req, res) => {
+ let y = await siteController.saveseedtrader(req, res)
+  if (r.user) {
+    res
+      .json({
+        statusCode: 200,
+        message: "Account created successfully",
+        body: r.seed_company
+      })
+      .send();
+  } else {
+    res
+      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .send();
+  }
+  }
+);
+
+
+
+module.exports = accountRouter;
--- src/routes/api/api.router.js
+++ src/routes/api/api.router.js
@@ -1,17 +1,15 @@
-const userRouter = require('./user.route');
-const farmerRouter = require('./farmer.router')
-const apiRouter = require('express').Router();
-const baseRouter = require('./base.router');
-const passport = require('passport');
-const passportjwt=require('../../helpers/passport-jwt')
-const verityToken = require('../../helpers/middleware');
-require('../../helpers/passport-jwt');
+const accountRouter = require("./account.route");
+const farmerRouter = require("./farmer.router");
+const apiRouter = require("express").Router();
+const baseRouter = require("./base.router");
+const passport = require("passport");
+const passportjwt = require("../../helpers/passport-jwt");
+const verityToken = require("../../helpers/middleware");
+require("../../helpers/passport-jwt");
 passportjwt(passport);
 //all router on api will be use here for prefixing
-apiRouter.use('/user', userRouter);
-apiRouter.use('/farmer', verityToken, farmerRouter);
-apiRouter.use('/base', baseRouter)
+apiRouter.use("/account", accountRouter);
+apiRouter.use("/farmer", verityToken, farmerRouter);
+apiRouter.use("/base", baseRouter);
 
-
-
-module.exports=apiRouter;
+module.exports = apiRouter;
