7ad89cf0c8e551f3741b54b3c460dcfe7b1d8f94
theRabbi <tipson664@gmail.com>
2022-08-05 12:32:45 +0100

    fix a bug on api accounts routes

--- src/routes/api/account.route.js
+++ src/routes/api/account.route.js
@@ -8,50 +8,57 @@ accountRouter.post("/login", async (req, res) => {
 
 accountRouter.post("/farmer-signup", async (req, res) => {
   let y = await siteController.savefarmer(req, res);
-  if (r.user) {
+
+  if (y.user) {
     res
+      .status(200)
       .json({
         statusCode: 200,
         message: "Account created successfully",
-        body: r.farmer,
+        body: y.farmer,
       })
       .send();
   } else {
     res
-      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .status(400)
+      .json({ statusCode: 400, message: "there was an error", body: y })
       .send();
   }
 });
 accountRouter.post("/company-signup", async (req, res) => {
   let y = await siteController.saveseedcompany(req, res);
-  if (r.user) {
+  if (y.user) {
     res
+      .status(200)
       .json({
         statusCode: 200,
         message: "Account created successfully",
-        body: r.seed_company,
+        body: y.seed_company,
       })
       .send();
   } else {
     res
-      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .status(400)
+      .json({ statusCode: 400, message: "there was an error", body: y })
       .send();
   }
 });
 
 accountRouter.post("/trader-signup", async (req, res) => {
   let y = await siteController.saveseedtrader(req, res);
-  if (r.user) {
+  if (y.user) {
     res
+      .status(200)
       .json({
         statusCode: 200,
         message: "Account created successfully",
-        body: r.seed_company,
+        body: y.seed_company,
       })
       .send();
   } else {
     res
-      .json({ statusCode: 400, message: "there was an error", body: r.e })
+      .status(400)
+      .json({ statusCode: 400, message: "there was an error", body: y })
       .send();
   }
 });
