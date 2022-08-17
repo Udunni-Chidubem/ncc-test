1384728bdc4e2706f32dbcdb2f780ad7d7c26408
DOC-ELIJAH <hopelijah98@gmail.com>
2022-08-17 12:35:29 +0100

    clear fixes

--- src/routes/api/api.router.js
+++ src/routes/api/api.router.js
@@ -1,19 +1,14 @@
 const userRouter = require('./user.route');
 const farmerRouter = require('./farmer.router')
 const apiRouter = require('express').Router();
-const baseRouter = require('./base.router');
+// const baseRouter = require('./base.router');
 const passport = require('passport');
 const passportjwt=require('../../helpers/passport-jwt')
 const verityToken = require('../../helpers/middleware');
 require('../../helpers/passport-jwt');
 //passportjwt(passport);
 const accountRouter = require("./account.route");
-// const farmerRouter = require("./farmer.router");
 // const apiRouter = require("express").Router();
-const baseRouter = require("./base.router");
-const passport = require("passport");
-const passportjwt = require("../../helpers/passport-jwt");
-const verityToken = require("../../helpers/middleware");
 require("../../helpers/passport-jwt");
 passportjwt(passport);
 //all router on api will be use here for prefixing
