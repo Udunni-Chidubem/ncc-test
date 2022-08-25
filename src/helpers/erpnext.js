59500931f1e0ecfb1b5a179e63fb7ac8eaed9d21
theRabbi <tipson664@gmail.com>
2022-08-25 09:30:51 +0100

    erp try catch

--- src/helpers/erpnext.js
+++ src/helpers/erpnext.js
@@ -2,17 +2,22 @@ const axios = require("axios").default;
 const FormData = require("form-data");
 module.exports = {
   login: async (username, password) => {
-    let form = new FormData();
-    form.append("usr", username);
-    form.append("pwd", password);
-    let resp = await axios.post(
-      "http://nigsimserp.interranetworks.com/api/method/library_management.api.login",
-      form,
-      {
-        headers: form.getHeaders(),
-      }
-    );
-    // console.log(resp);
-    return resp.data;
+    try {
+      let form = new FormData();
+      form.append("usr", username);
+      form.append("pwd", password);
+      let resp = await axios.post(
+        "http://nigsimserp.interranetworks.com/api/method/library_management.api.login",
+        form,
+        {
+          headers: form.getHeaders(),
+        }
+      );
+      // console.log(resp);
+      return resp.data;
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
   },
 };
