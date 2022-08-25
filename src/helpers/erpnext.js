8e4f213dc2ad53254e6544edfc49a6da46e373d8
theRabbi <tipson664@gmail.com>
2022-08-25 09:47:59 +0100

    erp try catch

--- src/helpers/erpnext.js
+++ src/helpers/erpnext.js
@@ -6,13 +6,14 @@ module.exports = {
       let form = new FormData();
       form.append("usr", username);
       form.append("pwd", password);
-      let resp = await axios.post(
-        "http://nigsimserp.interranetworks.com/api/method/library_management.api.login",
-        form,
-        {
-          headers: form.getHeaders(),
-        }
-      );
+      // let resp = await axios.post(
+      //   "http://nigsimserp.interranetworks.com/api/method/library_management.api.login",
+      //   form,
+      //   {
+      //     headers: form.getHeaders(),
+      //   }
+      // );
+      return "erp end point works";
       // console.log(resp);
       return resp.data;
     } catch (e) {
