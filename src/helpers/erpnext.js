077200290c1b2a2b4f3b609858dc066abb783752
theRabbi <tipson664@gmail.com>
2022-08-25 10:05:21 +0100

    erp try catch

--- src/helpers/erpnext.js
+++ src/helpers/erpnext.js
@@ -6,9 +6,9 @@ module.exports = {
       let form = new FormData();
       form.append("usr", username);
       form.append("pwd", password);
-      console.log(form);
+      //  console.log(form);
       let resp = await axios.post(
-        "https://nigsimserp.interranetworks.com/api/method/library_management.api.login",
+        "http://nigsimserp.interranetworks.com/api/method/library_management.api.login",
         form,
         {
           headers: form.getHeaders(),
