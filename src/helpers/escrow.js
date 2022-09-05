a49cf4073f205e9c7d5ec254f39fd99439657412
Ekene <ekeneumeh13@gmail.com>
2022-09-05 11:15:21 +0100

    escrow

--- src/helpers/escrow.js
+++ src/helpers/escrow.js
@@ -4,7 +4,24 @@ const axios = require("axios").default;
 
 module.exports = {
     
-
+    generateToken: async(tokenData) => {
+        let tokenData = {
+            "userIdentifyer": "INTERRANET",
+            "userProtector": "C@PaC0u@mY2KDF"
+        }
+        let resp = await axios.post(
+            "https://newwebservicetest.zenithbank.com:9443/directtransfer/api/transaction/zenithTransfer",
+            tokenData,
+            {
+              headers: {
+                "Content-Type":
+                  "application/json",
+              },
+            }
+        );
+        return resp;    
+    },
+    
     transferToZenith: async (transferData) => {
         let transferData = {
             amount: body.amount, 
@@ -14,8 +31,46 @@ module.exports = {
             drAccount: body.account,
             transactionRef:chjbksa
         }
+        let resp = axios.post(
+            "https://newwebservicetest.zenithbank.com:9443/directtransfer/api/transaction/zenithTransfer",
+            transferData,
+            {
+              headers: {
+                "Content-Type":
+                  "application/json",
+              },
+              authorization: {
+                  "Token": generateToken
+              },
+            }
+        );   
+        return resp; 
     },
 
-     
-    transferToOtherBank: async (xgcbx)
+
+    transferToOtherBank: async (transferData) => {
+        let transferData = {
+            amount: body.amount, 
+            bankName: body.bankName,
+            crAccount: body.account,
+            description: body, 
+            drAccount: process.env.escrowacct,
+            transactionRef:chjbksa
+        }
+        let resp = axios.post(
+            "https://newwebservicetest.zenithbank.com:9443/directtransfer/api/transaction/zenithTransfer",
+            transferData,
+            {
+              headers: {
+                "Content-Type":
+                  "multipart/form-data; boundary=<calculated when request is sent>",
+              },
+              authorization: {
+                  "Token": generateToken
+              },
+            }
+        );
+        return resp;    
+    },
+
 }
