e6413dbe52dbb67e433026c2d616f74a280f55f6
DOC-ELIJAH <hopelijah98@gmail.com>
2022-09-29 11:44:33 +0100

    conflicts

--- src/routes/api/api.router.js
+++ src/routes/api/api.router.js
@@ -1,12 +1,3 @@
-// const userRouter = require('./user.route');
-// const farmerRouter = require('./farmer.router')
-// const apiRouter = require('express').Router();
-// const baseRouter = require('./base.router');
-// const passport = require('passport');
-// const passportjwt=require('../../helpers/passport-jwt')
-// const verityToken = require('../../helpers/middleware');
-// require('../../helpers/passport-jwt');
-//passportjwt(passport);
 const accountRouter = require("./account.route");
 const farmerRouter = require("./farmer.router");
 const apiRouter = require("express").Router();
@@ -14,16 +5,11 @@ const baseRouter = require("./base.router");
 const passport = require("passport");
 const passportjwt = require("../../helpers/passport-jwt");
 const verityToken = require("../../helpers/middleware");
-const erpnext = require("../../helpers/erpnext");
 require("../../helpers/passport-jwt");
 passportjwt(passport);
 //all router on api will be use here for prefixing
 apiRouter.use("/account", accountRouter);
 apiRouter.use("/farmer", verityToken, farmerRouter);
 apiRouter.use("/base", baseRouter);
-apiRouter.post("/erp/login", async (req, res) => {
-  let result = await erpnext.login(req.body.username, req.body.password);
-  res.send(result);
-});
 
 module.exports = apiRouter;
