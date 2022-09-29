40c3968eb06df6229fc38dc6ab130f668e48caf9
DOC-ELIJAH <hopelijah98@gmail.com>
2022-09-29 11:40:42 +0100

    conflicts

--- src/routes/api/api.router.js
+++ src/routes/api/api.router.js
@@ -1,16 +1,13 @@
 const userRouter = require('./user.route');
-const farmerRouter = require('./farmer.router')
-const apiRouter = require('express').Router();
+// const farmerRouter = require('./farmer.router')
+// const apiRouter = require('express').Router();
 // const baseRouter = require('./base.router');
-const passport = require('passport');
-const passportjwt=require('../../helpers/passport-jwt')
-const verityToken = require('../../helpers/middleware');
-require('../../helpers/passport-jwt');
+// const passport = require('passport');
+// const passportjwt=require('../../helpers/passport-jwt')
+// const verityToken = require('../../helpers/middleware');
+// require('../../helpers/passport-jwt');
 //passportjwt(passport);
 const accountRouter = require("./account.route");
-<<<<<<< HEAD
-// const apiRouter = require("express").Router();
-=======
 const farmerRouter = require("./farmer.router");
 const apiRouter = require("express").Router();
 const baseRouter = require("./base.router");
@@ -18,7 +15,6 @@ const passport = require("passport");
 const passportjwt = require("../../helpers/passport-jwt");
 const verityToken = require("../../helpers/middleware");
 const erpnext = require("../../helpers/erpnext");
->>>>>>> 56bec5fb1df421fe817a0041b38cbb94935522d1
 require("../../helpers/passport-jwt");
 passportjwt(passport);
 //all router on api will be use here for prefixing
