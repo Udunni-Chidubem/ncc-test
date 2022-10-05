8ef16b6e6f2437c7ac2a3246e10b4d4269ac088f
theRabbi <tipson664@gmail.com>
2022-10-05 12:15:46 +0100

    new commit

--- src/routes/api/base.router.js
+++ src/routes/api/base.router.js
@@ -1,9 +1,13 @@
-const siteController = require('../../controllers/site.controller');
+const siteController = require("../../controllers/site.controller");
 
-const baseRouter = require('express').Router();
+const baseRouter = require("express").Router();
 
-baseRouter.get('/lgas/:state_id', siteController.lgaByStateId)
-baseRouter.get('/default-lga/:state_id/:lga_id', siteController.lgas)
-baseRouter.get('/get-droplist', siteController.getDropList)
+baseRouter.get("/lgas/:state_id", siteController.lgaByStateId);
+baseRouter.get("/default-lga/:state_id/:lga_id", siteController.lgas);
+baseRouter.get("/get-droplist", siteController.getDropList);
+baseRouter.get("/state", async (req, res) => {
+  let states = await siteController.getStates();
+  res.send(states);
+});
 
-module.exports = baseRouter
+module.exports = baseRouter;
