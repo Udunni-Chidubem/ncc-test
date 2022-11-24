4ac96b4bf1ea207eda390cd447d1b9445a963692
theRabbi <tipson664@gmail.com>
2022-11-24 13:16:48 +0100

    fix transaction bug

--- src/routes/api/api.router.js
+++ src/routes/api/api.router.js
@@ -6,12 +6,14 @@ const passport = require("passport");
 const passportjwt = require("../../helpers/passport-jwt");
 const verityToken = require("../../helpers/middleware");
 const companyRoute = require("./company.route");
+const traderRoute = require("./trader.route");
 require("../../helpers/passport-jwt");
 passportjwt(passport);
 //all router on api will be use here for prefixing
 apiRouter.use("/account", accountRouter);
 apiRouter.use("/farmer", verityToken, farmerRouter);
-apiRouter.use("/company", companyRoute);
+apiRouter.use("/company", verityToken, companyRoute);
+apiRouter.use("/trader", verityToken, traderRoute);
 apiRouter.use("/base", baseRouter);
 
 module.exports = apiRouter;
