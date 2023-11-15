b12caa87fe27ac755b8800806aa3ff9c35de9799
Ekene Umeh <ekeneumeh13@gmail.com>
2023-11-15 15:49:39 +0100

    migration for seed producer columns and seed producer seeds, update create seed producer screen and implementation, view seed producers content

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -525,9 +525,9 @@ module.exports = {
           // name_of_seed: req.body.nameOfSeed,
           // variety_of_seed: req.body.varietyOfSeed,
           // volume_of_seed: req.body.volumeOfSeed,
-          // gender: req.body.gender,
-          // age_range: req.body.age_range,
-          // living_status: req.body.living_status,
+          gender: req.body.gender,
+          age_range: req.body.age_range,
+          living_status: req.body.living_status,
           user_id: user.id,
           state_id: req.body.state_id,
           lg_id: req.body.lg_id,
@@ -569,6 +569,9 @@ module.exports = {
         {
           model: LGAs,
           attributes: ["name"],
+        },
+        {
+          model: SeedProducerSeed,
         }
       ],
       // raw: true,
