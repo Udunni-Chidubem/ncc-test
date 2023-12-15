499224a77fbd6ad90b9312528f72358d1fed45ff
Tochukwu Franklin Ene <teec3000@gmail.com>
2023-12-15 13:35:49 +0100

    Added report

--- src/routes/web/reports.router.js
+++ src/routes/web/reports.router.js
@@ -32,6 +32,7 @@ reportRouter.get("/transactions-jsp", async (req, res) => {
     result
   });
 });
+
 reportRouter.post("/transactions-jsp", async (req, res) => {
   let result = null;
   let Location = req.query.Location ?? "ALL";
@@ -53,5 +54,51 @@ reportRouter.post("/transactions-jsp", async (req, res) => {
   });
 });
 
+
+reportRouter.get("/user-report-jsp", async (req, res) => {
+  let user = await req.user;
+  let isVerified = await utils.isVerified(user);
+  let user_role = await adminController.getUserRole(req, res);
+  let states  = await adminController.getStates(req, res);
+  let result = null;
+  let Location = req.query.Location ?? "ALL";
+  let from = req.query.from ?? new Date().toISOString().split('T')[0];
+  let to = req.query.to ?? new Date().toISOString().split('T')[0];
+  let resp = await jasper.userReport(Location, from, to);
+  result = responses.success(resp);
+  console.log(result)
+
+  // res.status(result.code).send(result);
+
+  res.render("admin/user_report-jsp", {
+    layout: "admin-dashboard",
+    title: "User Jasper",
+    username: user.username,
+    isVerified,
+    user_role: user_role.Role.role_name,
+    states,
+    result
+  });
+});
+
+
+reportRouter.post("/user-report-jsp", async (req, res) => {
+  let result = null;
+  let Location = req.query.Location ?? "ALL";
+  let from = req.query.from;
+  let to = req.query.to;
+  let resp = await jasper.userReport(Location, from, to);
+  result = responses.success(resp);
+  res.status(result.code).send(result);
+  console.log(result)
+  res.render("admin/user_report-jsp", {
+    layout: "admin-dashboard",
+    title: "User Jasper",
+    username: user.username,
+    isVerified,
+    user_role: user_role.Role.role_name,
+    result
+  });
+});
 
 module.exports = reportRouter;
