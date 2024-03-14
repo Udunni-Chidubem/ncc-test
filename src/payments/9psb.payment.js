3038148d4bbf214ceed9929bc4b26488bb2741c3
codeshoel <softmaktech@gmail.com>
2024-03-14 11:01:45 +0100

    9psb checkout bug fixed

--- src/payments/9psb.payment.js
+++ src/payments/9psb.payment.js
@@ -68,189 +68,189 @@ module.exports = {
 
 
 
-    // otherCustomerValidate: async (req, res) => {
-    //     let r = req.body
-    //     let customerData = {
-    //         publickey: process.env.psb_publicKey,
-    //         source: {
-    //         operation: "account_enquiry",
-    //         recipient: {
-    //         accountnumber: r.account_number,
-    //         bankcode: r.bank_code
+    otherCustomerValidate: async (req, res) => {
+        let r = req.body
+        let customerData = {
+            publickey: process.env.psb_publicKey,
+            source: {
+            operation: "account_enquiry",
+            recipient: {
+            accountnumber: r.account_number,
+            bankcode: r.bank_code
             
-    //         }
-    //         },
-    //         order: {    
-    //         country: "NG"
-    //         }
-    //        }
-    //     try {
-    //     let token = await psb.generateToken()
-    //     if(token.code="00"){
-    //         let validation = await psb.validateOtherBank(customerData, token.access_token)
-    //         res.send(validation);
-    //     }else{
-    //         res.send(token)
-    //     }
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
+            }
+            },
+            order: {    
+            country: "NG"
+            }
+           }
+        try {
+        let token = await psb.generateToken()
+        if(token.code="00"){
+            let validation = await psb.validateOtherBank(customerData, token.access_token)
+            res.send(validation);
+        }else{
+            res.send(token)
+        }
+        } catch (e) {
+            console.log(e)
+        }
 
-    // },  
-    // getBanks: async (req, res) => {
+    },  
+    getBanks: async (req, res) => {
 
-    //     try {
-    //     let token = await psb.generateToken()
-    //     if(token.code="00"){
-    //         let banks = await psb.getAllBanks(token.access_token)
-    //         // console.log(banks)
-    //             res.send(banks);
-    //     }else{
-    //         res.send(token)
-    //     }
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
+        try {
+        let token = await psb.generateToken()
+        if(token.code="00"){
+            let banks = await psb.getAllBanks(token.access_token)
+            // console.log(banks)
+                res.send(banks);
+        }else{
+            res.send(token)
+        }
+        } catch (e) {
+            console.log(e)
+        }
 
-    // },
-    // psbAccountPayout: async (req, res) => {
-    //     let r = req.body
-    //     let data = {
+    },
+    psbAccountPayout: async (req, res) => {
+        let r = req.body
+        let data = {
             
-    //             publickey: process.env.psb_publicKey,
-    //              transaction: {
-    //              reference: uniqid()
-    //              },
-    //              order: {
-    //              amount: r.amount,
-    //              description: r.description,
-    //              reason: r.reason,
-    //              currency: "NGN",
-    //              country: "NG"
-    //              },
-    //              source: {
-    //              operation:"acct_payout",
-    //              sender: {
-    //              name: r.sender_name,
-    //              address: r.address,
-    //              mobile: r.phone_number,
-    //              country: "NG",
-    //              idtype: "PASSPORT",
-    //              idnumber: "P567839222",
-    //              idexpiry: "05-2019"
-    //              },
-    //              recipient: {
-    //              name: r.recipient_name,
-    //              address: r.recipient_address,
-    //              accountnumber: r.recipient_account,
-    //              bankcode: r.bank_code
-    //              }
-    //              }
+                publickey: process.env.psb_publicKey,
+                 transaction: {
+                 reference: uniqid()
+                 },
+                 order: {
+                 amount: r.amount,
+                 description: r.description,
+                 reason: r.reason,
+                 currency: "NGN",
+                 country: "NG"
+                 },
+                 source: {
+                 operation:"acct_payout",
+                 sender: {
+                 name: r.sender_name,
+                 address: r.address,
+                 mobile: r.phone_number,
+                 country: "NG",
+                 idtype: "PASSPORT",
+                 idnumber: "P567839222",
+                 idexpiry: "05-2019"
+                 },
+                 recipient: {
+                 name: r.recipient_name,
+                 address: r.recipient_address,
+                 accountnumber: r.recipient_account,
+                 bankcode: r.bank_code
+                 }
+                 }
                 
-    //     }    
-    //     try {
-    //         console.log("payload", data)
-    //     let token = await psb.generateToken()
-    //     //Get recipient bank code using their bank name 
-    //     // let allBankCode = await psb.getAllBanks(token.access_token)
-    //     // let b = allBankCode.banks
-    //     // let bank = await b.filter(e => {
-    //     //     return e.bankname==r.recipient_bank
-    //     // })
-    //     // console.log("recipient bank data", bank[0])
+        }    
+        try {
+            console.log("payload", data)
+        let token = await psb.generateToken()
+        //Get recipient bank code using their bank name 
+        // let allBankCode = await psb.getAllBanks(token.access_token)
+        // let b = allBankCode.banks
+        // let bank = await b.filter(e => {
+        //     return e.bankname==r.recipient_bank
+        // })
+        // console.log("recipient bank data", bank[0])
 
-    //     // bank_code = bank[0].bankcode
-    //     // console.log("recipient bank code", bank_code)
-    //     // data.recipient.bankcode = bank_code
-    //     if(token.code="00"){
-    //         let payout = await psb.psbPayout(data, token.access_token)
-    //         console.log(payout)
-    //             res.send(payout);
-    //     }else{
-    //         res.send(token)
-    //     }
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
+        // bank_code = bank[0].bankcode
+        // console.log("recipient bank code", bank_code)
+        // data.recipient.bankcode = bank_code
+        if(token.code="00"){
+            let payout = await psb.psbPayout(data, token.access_token)
+            console.log(payout)
+                res.send(payout);
+        }else{
+            res.send(token)
+        }
+        } catch (e) {
+            console.log(e)
+        }
 
-    // },
-    // otherBankPayout: async (req, res) => {
-    //     let r = req.body
-    //     let data = {
+    },
+    otherBankPayout: async (req, res) => {
+        let r = req.body
+        let data = {
             
-    //             publickey: process.env.psb_publicKey,
-    //              transaction: {
-    //              reference: uniqid()
-    //              },
-    //              order: {
-    //              amount: r.amount,
-    //              description: r.description,
-    //              reason: r.reason,
-    //              currency: "NGN",
-    //              country: "NG"
-    //              },
-    //              source: {
-    //              operation:"acct_payout",
-    //              sender: {
-    //              name: r.sender_name,
-    //              address: r.address,
-    //              mobile: r.phone_number,
-    //              country: "NG",
-    //              idtype: "PASSPORT",
-    //              idnumber: "P567839222",
-    //              idexpiry: "05-2019"
-    //              },
-    //              recipient: {
-    //              name: r.recipient_name,
-    //              address: r.recipient_address,
-    //              accountnumber: r.recipient_account,
-    //              bankcode: r.bank_code
-    //              }
-    //              }
+                publickey: process.env.psb_publicKey,
+                 transaction: {
+                 reference: uniqid()
+                 },
+                 order: {
+                 amount: r.amount,
+                 description: r.description,
+                 reason: r.reason,
+                 currency: "NGN",
+                 country: "NG"
+                 },
+                 source: {
+                 operation:"acct_payout",
+                 sender: {
+                 name: r.sender_name,
+                 address: r.address,
+                 mobile: r.phone_number,
+                 country: "NG",
+                 idtype: "PASSPORT",
+                 idnumber: "P567839222",
+                 idexpiry: "05-2019"
+                 },
+                 recipient: {
+                 name: r.recipient_name,
+                 address: r.recipient_address,
+                 accountnumber: r.recipient_account,
+                 bankcode: r.bank_code
+                 }
+                 }
                 
-    //     }    
-    //     try {
-    //         console.log("payload", data)
-    //     let token = await psb.generateToken()
-    //     if(token.code="00"){
-    //         let payout = await psb.otherPayout(data, token.access_token)
-    //         console.log(payout)
-    //             res.send(payout);
-    //     }else{
-    //         res.send(token)
-    //     }
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
+        }    
+        try {
+            console.log("payload", data)
+        let token = await psb.generateToken()
+        if(token.code="00"){
+            let payout = await psb.otherPayout(data, token.access_token)
+            console.log(payout)
+                res.send(payout);
+        }else{
+            res.send(token)
+        }
+        } catch (e) {
+            console.log(e)
+        }
 
-    // },
-    // payoutStatus: async (req, res) => {
-    //     let ref = req.params.ref
-    //     try {
-    //     let token = await psb.generateToken()
-    //     if(token.code="00"){
-    //         let status = await psb.payoutStatus(ref, token.access_token)
-    //         console.log(status)
-    //             res.send(status);
-    //     }else{
-    //         res.send(token)
-    //     }
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
+    },
+    payoutStatus: async (req, res) => {
+        let ref = req.params.ref
+        try {
+        let token = await psb.generateToken()
+        if(token.code="00"){
+            let status = await psb.payoutStatus(ref, token.access_token)
+            console.log(status)
+                res.send(status);
+        }else{
+            res.send(token)
+        }
+        } catch (e) {
+            console.log(e)
+        }
 
-    // },
+    },
 
-    // //PAYMENT GATEWAY 
-    // authentication: async (req, res) => {
-    //     let token = await psb.gatewayTokenGeneration()
-    //     res.send(token)
-    //     return token
-    // },
+    //PAYMENT GATEWAY 
+    authentication: async (req, res) => {
+        let token = await psb.gatewayTokenGeneration()
+        res.send(token)
+        return token
+    },
 
-    // initializeTransaction: async(email, amount, callback, req) => {
-    //     let token = await psb.gatewayTokenGeneration()
-    //     let initial = await psb.initialize(email, amount, callback, token, req)
-    //     return initial
-    // }
+    initializeTransaction: async(email, amount, callback, req) => {
+        let token = await psb.gatewayTokenGeneration()
+        let initial = await psb.initialize(email, amount, callback, token, req)
+        return initial
+    }
 }
